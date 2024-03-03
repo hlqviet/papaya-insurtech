235d6f0ac3002287910e91a9dc9ffde4ccf76d7f
Viet Huynh <xa.lach@yahoo.com>
2024-03-04 03:46:52 +0700

    implemented logout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,14 +4,25 @@ import useAuth from '../../hooks/useAuth/useAuth'
 
 const Layout = (props: PropsWithChildren<{ title: string }>) => {
   const { children, title } = props
-  const { user } = useAuth()
+  const { user, logout } = useAuth()
 
   return (
     <>
       <main className='prose'>
         <header>
           <h1>{title}</h1>
-          {user && <span>Hi {`${user.firstName} ${user.lastName}`}</span>}
+          {user && (
+            <div className='flex flex-col my-4'>
+              <span>Hi {`${user.firstName} ${user.lastName}`}!</span>
+              <span>
+                Don&apos;t like this user?{' '}
+                <button className='btn-link' onClick={logout}>
+                  Logout
+                </button>
+                .
+              </span>
+            </div>
+          )}
         </header>
         <section>{children}</section>
       </main>
